95472bc82404a1aeffd52ecb1d1f549251d664a0
Sy Tran Dung <sytrancsvn@gmail.com>
2023-06-14 01:29:11 +0700

    build with cmake

--- cpp/regular-expression-matching.cpp
+++ cpp/regular-expression-matching.cpp
@@ -1,43 +1,42 @@
-#ifdef DEBUG
-#include<bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
-#endif
 
 class Solution {
 public:
-    bool match(char a, char b) {
-        return a == b || b == '.';
-    }
-    bool isMatch(string s, string p) {
-        int i = 0 , j = 0;
-        bool lastMatched = false;
-        char exp;
-        while (i < s.length() && j < p.length()) {
-            if (p[j] != '*') {
-                exp = p[j];
-            if (match(s[i], exp)) {
-                i++;
-            } else {
-                return false;
-            }
-            } else {
-                while (match(s[i], exp)) {
-                    i++;
-                }
-                j++;
-            }
+  bool match(char a, char b) { return a == b || b == '.'; }
+  bool isMatch(string s, string p) {
+    int i = 0, j = 0;
+    bool lastMatched = false;
+    char exp;
+    while (i < s.length() && j < p.length()) {
+      if (p[j] != '*') {
+        exp = p[j];
+        if (match(s[i], exp)) {
+          i++;
+        } else {
+          return false;
         }
-
-
-        return true;
+      } else {
+        while (match(s[i], exp)) {
+          i++;
+        }
+        j++;
+      }
     }
+
+    return true;
+  }
 };
 
-#ifdef DEBUG
 int main() {
-    Solution s = Solution();
-    cout << s.isMatch("ab", ".*") << endl;
-    cout << s.isMatch("ab", "a*") << endl;
-    cout << s.isMatch("abb", ".*b") << endl;
+  Solution s = Solution();
+  string s1, s2;
+  while (true) {
+    getline(cin, s1);
+    if (s1.empty())
+      break;
+    getline(cin, s2);
+    s.isMatch(s1, s2);
+  }
 }
-#endif
