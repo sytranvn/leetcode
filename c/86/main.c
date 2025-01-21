7d30b54233adc245791a1738c79066a98b787744
Sy Tran Dung <sytrancsvn@gmail.com>
2025-01-21 15:06:03 +0700

    fix conflicts

--- c/86/main.c
+++ c/86/main.c
@@ -1,49 +1,5 @@
 #include "../include/leetcode.h"
 
-<<<<<<< HEAD
-struct ListNode* partition(struct ListNode* head, int x) {
-    struct ListNode lessDummy = {0, NULL};
-    struct ListNode greaterDummy = {0, NULL};
-    struct ListNode* less = &lessDummy;
-    struct ListNode* greater = &greaterDummy;
-    printf("partitioning\n");
-    while (head != NULL) {
-        if (head->val < x) {
-            less->next = head;
-            less = less->next;
-        } else {
-            greater->next = head;
-            greater = greater->next;
-        }
-        head=head->next;
-    }
-    greater->next = NULL;
-    less->next = greaterDummy.next;
-    return lessDummy.next;
-}
-
-int main() {
-    printf("Hello\n");
-    int arr[] = {1,4,3,2,5,2};
-    int expect[] = {1,2,2,4,3,5};
-    struct ListNode l;
-    struct ListNode *c = &l;
-    for (int i = 0; i < 6; i++) {
-        c->val = arr[i];
-        struct ListNode n = {0, NULL};
-        c->next = &n;
-        c = c->next;
-    }
-    struct ListNode *out = partition(&l, 3);
-    printf("partitioned\n");
-    c = out; int i=0;
-    while (c != NULL) {
-        assert(c->val==expect[i++]);
-        c = c->next;
-    }
-    
-    return 0;
-=======
 struct ListNode *partition(struct ListNode *head, int x) {
   struct ListNode lessDummy = {0, NULL};
   struct ListNode greaterDummy = {0, NULL};
@@ -87,5 +43,4 @@ int main() {
   }
 
   return 0;
->>>>>>> 92a073f (refactor)
 }
