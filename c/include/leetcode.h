3d11f40cf52c0833ff83425ddbe798e30e85b320
Sy Tran Dung <sytrancsvn@gmail.com>
2025-01-21 15:15:07 +0700

    array helper

--- c/include/leetcode.h
+++ c/include/leetcode.h
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include <stdio.h>
@@ -11,5 +12,42 @@ struct ListNode {
   int val;
   struct ListNode *next;
 };
+
+
+int* parse_array(const char* str, int* size) {
+    int* array = NULL;
+    int count = 0;
+    const char* p = str;
+
+    while (*p) {
+        if (*p >= '0' && *p <= '9') {
+            array = realloc(array, (count + 1) * sizeof(array));
+            array[count++] = strtol(p, (char**)&p, 10);
+        } else {
+            p++;
+        }
+    }
+
+    *size = count;
+    return array;
+}
+
+long long* parse_array_ll(const char* str, int* size) {
+    long long* array = NULL;
+    int count = 0;
+    const char* p = str;
+
+    while (*p) {
+        if (*p >= '0' && *p <= '9') {
+            array = realloc(array, (count + 1) * sizeof(array));
+            array[count++] = strtoll(p, (char**)&p, 10);
+        } else {
+            p++;
+        }
+    }
+
+    *size = count;
+    return array;
+}
 
 #endif
